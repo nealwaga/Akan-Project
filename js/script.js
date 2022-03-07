4a79741ab1591c9e9ff7d1fe9bd9f55f046dbbbb
nealwaga <waganeal@gmail.com>
2022-03-08 02:17:20 +0300

    new method

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function getIndex(userDate) {
     const date = new Date(userDate);
 
-    if(date.getMonth() <=0 || date.getMonth() >)
+    if(date.getMonth() <=0 || date.getMonth() > 12){
+        console.log("error!")
+    }
 }
